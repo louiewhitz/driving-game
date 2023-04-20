1a55de9ab733a93a6f03b9702c0d3286226a8ca5
Louisa Whitaker <louisa.whitaker@gmail.com>
2023-04-20 04:11:22 +0000

    working on positioning of car on start and movement - need to add translate instead of setTimeout

--- main.js
+++ main.js
@@ -19,22 +19,71 @@ const carHeight = car.offsetHeight;
 let carX = containerWidth / 2;
 let carY = containerHeight / 2;
 
-// set up the turn animation
-const turnDuration = {
-  duration: 150,
-  fill: 'forwards'
-};
+// update the car's position and handle turns every 16ms (approximately 60fps)
+// const move = setInterval(() => {
+//   // only update the car's position if it's currently on
+
+// }, 16);
 
 // start the car when the space bar is pressed
 document.addEventListener('keydown', startCar);
 function startCar(event) {
-  if (event.code === 'Space' && !data.on) {
+  if (event.code === 'Space') {
+    // eslint-disable-next-line no-console
+    console.log('Space pressed');
+    if (data.on) {
+      // update the car's position based on the direction
+      if (data.direction === 'north') {
+        carY -= 3;
+      } else if (data.direction === 'south') {
+        carY += 3;
+      } else if (data.direction === 'east') {
+        carX += 3;
+      } else if (data.direction === 'west') {
+        carX -= 3;
+      }
+
+      // update the car's position using absolute positioning
+      car.style.position = 'absolute';
+      car.style.top = carY + 'px';
+      car.style.left = carX + 'px';
+
+      // check if the car has reached the edge of the container and turn it
+      if (carY <= 0) {
+        data.on = false;
+      } else if (carY >= containerHeight - carHeight) {
+        data.on = false;
+      } else if (carX <= 0) {
+        data.on = false;
+      } else if (carX >= containerWidth - carWidth) {
+        data.on = false;
+      } else {
+        // continue moving in the same direction if the edge hasn't been reached
+        const direction = car.className;
+        switch (direction) {
+          case 'north':
+            data.direction = 'north';
+            break;
+          case 'south':
+            data.direction = 'south';
+            break;
+          case 'west':
+            data.direction = 'west';
+            break;
+          case 'east':
+            data.direction = 'east';
+            break;
+        }
+      }
+    }
     data.on = true;
+    data.direction = car.className;
+    // set a default direction
   }
 }
 
 // turn the car when the arrow keys are pressed
-document.addEventListener('keydown', turnCar);
+
 function turnCar(event) {
   switch (event.key) {
     case 'ArrowUp':
@@ -54,50 +103,4 @@ function turnCar(event) {
   }
 }
 
-// update the car's position every 16ms (approximately 60fps)
-setInterval(() => {
-  // only update the car's position if it's currently on
-  if (data.on) {
-    // get the current direction of the car
-    const direction = car.className;
-
-    // update the car's position based on its direction
-    switch (direction) {
-      case 'north':
-        if (carY <= 0) {
-          data.on = false;
-          break;
-        }
-        car.animate([{ transform: `translateY(-${carHeight}px)` }], turnDuration);
-        carY -= carHeight;
-        break;
-      case 'south':
-        if (carY >= carHeight) {
-          data.on = false;
-          break;
-        }
-        car.animate([{ transform: `translateY(${carHeight}px)` }], turnDuration);
-        carY += carHeight;
-        break;
-      case 'west':
-        // check if the car has reached the left edge of the container
-        if (carX <= 0) {
-          data.on = false;
-          break;
-        }
-        car.animate([{ transform: `translateX(-${carWidth}px)` }], turnDuration);
-        carX -= carWidth;
-        break;
-      case 'east':
-        // check if the car has reached the right edge of the container
-        if (carX + carWidth >= containerWidth) {
-          data.on = false;
-          break;
-        }
-        car.animate([{ transform: `translateX(${carWidth}px)` }], turnDuration);
-        carX += carWidth;
-        break;
-
-    }
-  }
-});
+document.addEventListener('keydown', turnCar);
